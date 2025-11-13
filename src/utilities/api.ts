50294ddf17fb5f5bfaa1c71fb5b85a73fd305bf4
James Randall <james@manmachineltd.com>
2025-11-13 19:49:34 +1100

    updated api error logging

--- src/utilities/api.ts
+++ src/utilities/api.ts
@@ -9,15 +9,14 @@ const DEFAULT_HEADERS = {
 } as const
 
 export const handleApiError = async (e: AxiosError) => {
-  console.error(e)
+  console.log('API Error: ', e)
   throw new Error(e?.message)
 }
 
 export const handleValidationError = async (e: AxiosError) => {
   const data = e?.response?.data as LaravelErrorResponse | undefined
-  if (!data) return
-  const message = data?.message
-  console.error(message)
+  if (!data) throw new Error(e?.message || 'Validation Error')
+  console.log('API Validation Error: ', e)
 }
 
 export const api = async (url: string, config: AxiosRequestConfig = {}) => {
@@ -30,7 +29,7 @@ export const api = async (url: string, config: AxiosRequestConfig = {}) => {
       ...config.headers,
     },
   }
-  return axios.request(mapped).catch((e: AxiosError) => {
+  return await axios.request(mapped).catch((e: AxiosError) => {
     if (e?.status === 422) handleValidationError(e)
     else handleApiError(e)
   })
